34945c8fc0ead6ffc2e409749414e3e2fe140797
AlinaBabenko02 <your.alinaa@gmail.com>
2023-12-01 01:04:31 +0100

    fix: input styling

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,23 +3,11 @@ import { ConfigProvider, Space } from "antd";
 import { StarryBackground } from "./components/starry-background/StarryBackground";
 import { CharactersListScreen } from "./screens/characters-list/CharactersListScreen";
 import { CharacterScreen } from "./screens/character/CharacterScreen";
+import { antdTheme } from "../data/theme";
 
 function App() {
   return (
-    <ConfigProvider
-      theme={{
-        token: {
-          colorPrimary: "#1f1f3a",
-          borderRadius: 8,
-          fontSize: 18,
-        },
-        components: {
-          Input: {
-            paddingBlock: 8,
-          },
-        },
-      }}
-    >
+    <ConfigProvider theme={antdTheme}>
       <BrowserRouter>
         <StarryBackground />
         <Routes>
---
+++ src/data/theme.ts
@@ -0,0 +1,13 @@
+export const antdTheme = {
+  token: {
+    colorPrimary: "#1f1f3a",
+    borderRadius: 8,
+    fontSize: 18,
+    controlHeight: 44,
+  },
+  components: {
+    Input: {
+      paddingBlock: 8,
+    },
+  },
+};
